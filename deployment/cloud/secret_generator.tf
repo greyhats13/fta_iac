523bef5089dab9de848e4b89c8b87ff5d14fa89e
greyhats13 <imam.arief.rhmn@gmail.com>
2024-09-17 15:36:20 +0700

    remove lifecycle ignore changes

--- deployment/cloud/secret_generator.tf
+++ deployment/cloud/secret_generator.tf
@@ -5,9 +5,6 @@ resource "random_password" "atlantis_github_secret" {
   min_upper        = 10
   min_numeric      = 10
   min_special      = 5
-  lifecycle {
-    ignore_changes = [result]
-  }
 }
 
 resource "random_password" "atlantis_password" {
@@ -17,15 +14,9 @@ resource "random_password" "atlantis_password" {
   min_upper        = 3
   min_numeric      = 3
   min_special      = 0
-  lifecycle {
-    ignore_changes = [result]
-  }
 }
 
 resource "tls_private_key" "atlantis_ssh" {
   algorithm = "RSA"
   rsa_bits  = 2048
-  lifecycle {
-    ignore_changes = [result]
-  }
 }
